16bad3528a1cfff64b08a89fd9c22a813dc086c9
xrlu0929 <xl2474@tc.columbia.edu>
2018-03-28 22:43:51 -0400

    first time commit

---
+++ Section_02/BullCowGame/BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Xinrui Lu on 3/28/18.
+//  Copyright © 2018 AXL. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
